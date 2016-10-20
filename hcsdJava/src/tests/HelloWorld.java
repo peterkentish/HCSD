5ed874ef9c47e1ed7357f9915f47673edb02fd58
Georgia Hardy <ghardy1@sheffield.ac.uk>
2016-10-20 10:45:30 +0100

    Peter is not a twat test

--- hcsdJava/src/tests/HelloWorld.java
+++ hcsdJava/src/tests/HelloWorld.java
@@ -3,6 +3,6 @@ package tests;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("HELLO WORLD");
-		System.out.println("PETER IS A TWAT");
+		System.out.println("PETER IS NOT A TWAT");
 	}
 }
