130fcdbb93c86c0c62c52bee0f57100d602fc059
Daniel Solomon <danielsolo@hotmail.co.uk>
2016-10-29 22:55:51 +0100

    to string

--- hcsdJava/src/Database/Patient.java
+++ hcsdJava/src/Database/Patient.java
@@ -8,13 +8,33 @@ public class Patient {
 	private String healthCare;
 	
 	public Patient(int patientID, String firstName, String surname, String healthCare){
+		super();
 		this.patientID = patientID;
 		this.firstName = firstName;
 		this.surname = surname;
 		this.healthCare = healthCare;
 		
-		srhgfh
+		
 		
 	}
 
+	public int getID(){
+		return patientID;
+	}
+	
+	public String getFirst(){
+		return firstName;
+	}
+	public String getSurname(){
+		return surname;
+	}
+	public String getHealthCare(){
+		return healthCare;
+	}
+	
+	public String toString() {
+		return String
+				.format("%s, %s ,%s, %s\n",patientID, firstName, surname, healthCare);
+	}
+	
 }
