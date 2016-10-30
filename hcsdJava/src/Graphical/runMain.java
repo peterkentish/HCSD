e6af6f60fd1f160d57981877808103a9eac2eaf6
Daniel Solomon <danielsolo@hotmail.co.uk>
2016-10-30 01:38:30 +0100

    search gui

--- hcsdJava/src/Graphical/runMain.java
+++ hcsdJava/src/Graphical/runMain.java
@@ -1,14 +1,16 @@
 package Graphical;
-
+import Database.Database;
 import javax.swing.JFrame;
 
 public class runMain {
+	static Database db = new Database();
 	public static void main(String[] args){
 		
 		mainMenu run = new mainMenu();
 		run.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		run.setSize(1000,1000);
 		run.setVisible(true);
+		System.out.println(db.selectPatient("*","patient_table","patient_first_name='Daniel'"));
 
 	}
 	
