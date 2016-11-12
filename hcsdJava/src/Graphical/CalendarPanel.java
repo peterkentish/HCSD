73714ea542c2469c5b82116ca73a0bd9c05f8007
peter kentish <pkentishdelaiglesia1@sheffield.ac.uk>
2016-11-12 19:36:45 +0000

    changing cal

--- hcsdJava/src/Graphical/CalendarPanel.java
+++ hcsdJava/src/Graphical/CalendarPanel.java
@@ -4,16 +4,47 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import javax.swing.JPanel;
 
 public class CalendarPanel extends JPanel {
-	Calendar c =  Calendar.getInstance();
+	
+	
 	
 	String[] dayOfWeek = new String[7];
+	List<java.sql.Time> times = new ArrayList<>();
+	ArrayList<String> timesString = new ArrayList<String>();
+	Date weekStart;
+	
+	public void getStartOfWeek(){
+		Calendar c = Calendar.getInstance();
+		c.setFirstDayOfWeek(Calendar.MONDAY);
+		c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+		weekStart = c.getTime();
+		int weekStartInt = weekStart.getDate();
+		int monthInt = weekStart.getMonth();
+	}
+	public void populateTimesOfDay(){
+		java.sql.Time startTime = new java.sql.Time(9, 0, 0);
+		java.sql.Time endTime = new java.sql.Time(17, 0, 0);
+
+		times.add(startTime);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(startTime);
+		while (cal.getTime().before(endTime)) {
+		    cal.add(Calendar.MINUTE, 20);
+		    times.add(new java.sql.Time(cal.getTimeInMillis()));
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+		for (java.sql.Time time : times) {
+		    timesString.add(sdf.format(time));
+		}
+	}
 	public void populateDaysOfWeek(){
 		dayOfWeek[0]="Monday";
 		dayOfWeek[1]="Tuesday";
@@ -25,15 +56,28 @@ public class CalendarPanel extends JPanel {
 	}
 	public CalendarPanel(){
 		setBackground(Color.WHITE);	
+		populateTimesOfDay();
+		populateDaysOfWeek();
+		getStartOfWeek();
 	}
 	public void paintComponent(Graphics graphics){
 		super.paintComponent(graphics);
 		Graphics2D g = (Graphics2D) graphics;
-		populateDaysOfWeek();
 		Font mainFont = new Font("Century Gothic", 0, 20);
+		Font titleFont = new Font("Century Gothic", 0, 28);
+		
+		g.setFont(titleFont);
+		g.drawString("Week Commencing "+weekStart.toString().substring(4, 10), 400, 25);
+		
 		g.setFont(mainFont);
 		for (int i=0;i<dayOfWeek.length;i++){
 			g.drawString(dayOfWeek[i], 160+150*i, 50);
+			g.drawLine(135+150*i, 50, 135+150*i, this.getHeight());
 		}
+		for (int i=0;i<times.size();i++){
+			g.drawString(timesString.get(i), 40, 70+23*i);
+			g.drawLine(40,72+23*i,this.getWidth(),72+23*i);
+		}
+		
 	}
 }
--- hcsdJava/src/Graphical/SecretaryMain.java
+++ hcsdJava/src/Graphical/SecretaryMain.java
@@ -1,24 +1,35 @@
 package Graphical;
 
-import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 public class SecretaryMain extends DefaultFrame{
 	
 	public SecretaryMain(){
-		setTitle("Secretary");
-		Container contentPane = this.getContentPane();
-		TitlePanel title = new TitlePanel("Secretary");
-		title.setPreferredSize(new Dimension(200,100));
-		CalendarPanel cal = new CalendarPanel();
-		cal.setPreferredSize(new Dimension(200,100));
-		//Add everything to the frame space
-		contentPane.setLayout(new BorderLayout());
-		contentPane.add(cal, BorderLayout.CENTER);
-		contentPane.add(title, BorderLayout.NORTH);
+//		setTitle("Secretary");
+//		title.setPreferredSize(new Dimension(200,100));
+		Container contentPane = getContentPane();
+		
+		CalendarPanel dentistCal = new CalendarPanel();
+		CalendarPanel hygieneCal = new CalendarPanel();
+		RegistrationInformationPanel regPan = new RegistrationInformationPanel();
+//		JTabbedPane tabs = new JTabbedPane();
+//		tabs.addTab("Diary", cal);
+//		//Add everything to the frame space
+//		contentPane.setLayout(new BorderLayout());
+//		contentPane.add(cal, BorderLayout.CENTER);
+//		contentPane.add(title, BorderLayout.NORTH);
+//		
+		
+	      setTitle("Secretary");
+	      JTabbedPane jtp = new JTabbedPane();
+	      contentPane.add(jtp);
+	      
+	      jtp.addTab("Dentist Week Schedule", dentistCal);
+	      jtp.addTab("Hygiene Week Schedule", hygieneCal);
+          jtp.addTab("Register a new Patient", regPan);
 		
 	}
 	public static void main(String[] args) {
