91455ec306f2f91c3a8c540c548787196c1e94c7
Georgia Hardy <ghardy1@sheffield.ac.uk>
2016-11-21 13:50:57 +0000

    sucks balls

--- hcsdJava/src/Graphical/CalendarPanelDay.java
+++ hcsdJava/src/Graphical/CalendarPanelDay.java
@@ -1,5 +1,12 @@
-package Graphical;
+/*
+ * A calendar which uses graphics 2D to produce a table, with 1 columns.
+ * the first column being the time in 20 mintue intervals, the other being
+ * the day of the week. It then populates these
+ * with the data from the database for each day and time segment. 
+ * for extensive comments see CalendarPanel as it is based off of that 
+ */
 
+package Graphical;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -22,13 +29,7 @@ import javax.swing.JPanel;
 import Database.Appointment;
 import Database.Database;
 import Database.Patient;
-/*
- * A calendar which uses graphics 2D to produce a table, with 1 columns.
- * the first column being the time in 20 mintue intervals, the other being
- * the day of the week. It then populates these
- * with the data from the database for each day and time segment. 
- * for extensive comments see CalendarPanel as it is based off of that 
- */
+
 
 public class CalendarPanelDay extends JPanel implements ActionListener {
 	
@@ -42,7 +43,7 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 	Date today;
 	
 	Database db = new Database();
-	
+	//get the start of the week, using the most recent monday as the start
 	public void getStartOfWeek(int i){
 		Calendar c = Calendar.getInstance();
 		today = c.getTime();
@@ -51,6 +52,7 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 		today.setMinutes(1);
 		int monthInt = today.getMonth();
 	}
+	//create the times in 20 minute intervals, only allow the end time after start time
 	public void populateTimesOfDay(){
 		java.sql.Time startTime = new java.sql.Time(9, 0, 0);
 		java.sql.Time endTime = new java.sql.Time(17, 0, 0);
@@ -67,6 +69,7 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 		}
 	}
 
+	//constructor that gets the appointments from the db and adds them to arraylist apps.
 	public CalendarPanelDay(String staffS){
 		this.staff = staffS;
 		setBackground(Color.WHITE);	
@@ -83,6 +86,8 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 		this.add(next);
 		next.setBounds(200, 100, 50, 50);
 	}
+
+	//where the calendar day is drawn
 	public void paintComponent(Graphics graphics){
 		super.paintComponent(graphics);
 		Graphics2D g = (Graphics2D) graphics;
@@ -96,13 +101,14 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 		ArrayList<Date> appTimes = new ArrayList<Date>();
 		ArrayList<Patient> patients = new ArrayList<Patient>();
 		
+		//draw the axes labels
 		g.setFont(mainFont);
 		AffineTransform gSave = g.getTransform();
 		g.rotate(-Math.PI/2);
 		g.drawString("Appointment Start", -400, 30);
 		g.setTransform(gSave);
 		
-		
+		//if there are appointments add them to apptimes, an arraylist of appointment times
 		if (apps != null) {
 			for (Appointment a : apps) {
 				appTimes.add(stringToDate(a.getStartTime()));
@@ -113,17 +119,24 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 			}
 		}
 		
+		//draw the vertical axis line
 		g.drawLine(110, 40,110, this.getHeight());
+		//a loop to draw the times throughout the day separated by 20 minute intervals
 		for (int i=0;i<times.size();i++){
 			int yValue = 70+23*i;
 			g.drawString(timesString.get(i), 40, 70+23*i);
 			g.drawLine(40,yValue,this.getWidth(),yValue);
 			if (apps!=null) {
+				//for every other element in apptimes
 				for (int j = 0; j < appTimes.size(); j = j + 2) {
+					//if the current appTimes start is equal to the horizontal table element we are in 
+					//print the appt. So if current time is 9:00 and we have an appointment at 9:00 in
+					//the apps list, print it to the current space. Also, get the appt length in units of 20.
 					if (getHoursMins(times.get(i)).equals(
 							getHoursMins(appTimes.get(j)))) {
 						int apptLength = getDifference(appTimes.get(j),
 								appTimes.get(j + 1)) / 20;
+						// for the length of the appointment, fill in the z amount of appointment spaces.
 						for (int z = 0; z < apptLength; z++) {
 							Patient current = patients.get(j/2);
 							g.drawString(current.getTitle()+" "+current.getFirstName()+" "+current.getLastName(),
@@ -135,10 +148,13 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 			}
 		}
 	}
+
+	//format a date to sql date format
 	public String sqlFormatterToday(Date d){
 		String x = "'"+(d.getYear()+1900)+"-"+(d.getMonth()+1)+"-"+d.getDate()+" "+d.getHours()+":"+d.getMinutes()+":00' ";
 		return x;
 	}
+	//turn a string into a date
 	  public Date stringToDate(String s){
 		  	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		    String startDateString = s;
@@ -155,6 +171,7 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 		    }
 		    return newerDate;
 	  }
+	  //return just the hours and mins of a date as a string
 	  public String getHoursMins(Date d){
 		 Integer h = d.getHours();
 		 Integer m = d.getMinutes();
@@ -162,6 +179,7 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 		 return x;
 		  
 	  }
+	  //get the time difference between two times using the expansion of minutes.
 	  public int getDifference(Date date1, Date date2) {
 		    int numHours1 =date1.getHours();
 		    int numMinutes1 = date1.getMinutes();
@@ -173,6 +191,7 @@ public class CalendarPanelDay extends JPanel implements ActionListener {
 		    return total;
 		}
 	@Override
+	//buttons for increasing and decreasing of the date
 	public void actionPerformed(ActionEvent e) {
 
 		int current = today.getDate();
